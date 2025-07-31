519066c12b5dd0798f84c4640d2263b8ba9b6304
yejunian <leeye51456@gmail.com>
2025-07-31 21:41:58 +0900

    ✨ feat: 기본 형태 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import styles from "./App.module.css";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className={styles.app}>
+      <h1>웹 OMR 시뮬레이터</h1>
+
+      <div className={styles.omr}>
+        <div className={styles.header}>
+          <div>문번</div>
+          <div>답란</div>
+        </div>
+
+        <div className={styles.answer}>
+          {Array.from({ length: 45 }).map((_, index) => (
+            <div key={index + 1} className={styles.row}>
+              <div className={styles.question}>{index + 1}</div>
+              <button>1</button>
+              <button>2</button>
+              <button>3</button>
+              <button>4</button>
+              <button>5</button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
