8e4f9f5aa6aca34d5aa8555002e7f36788abf576
yejunian <leeye51456@gmail.com>
2025-08-02 23:56:46 +0900

    🐛 fix: Vite path alias 지정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+import { resolve } from "node:path";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: resolve(__dirname, "./src"),
+      },
+    ],
+  },
+
   plugins: [react()],
 });
